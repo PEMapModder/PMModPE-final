93314c76d1eca2cf3bdb3d9c13cb211033138b11
PEMapModder <pemapmodder1970@gmail.com>
2014-06-28 10:45:07 +0800

    Update Main.php

--- PMModPE/src/pemapmodder/pmmodpe/Main.php
+++ PMModPE/src/pemapmodder/pmmodpe/Main.php
@@ -5,6 +5,12 @@ namespace pemapmodder\pmmodpe\Main;
 use pocketmine\plugin\PluginBase;
 
 class Main extends PluginBase{
+	private $cacheDir;
 	public function onEnable(){
+		@mkdir($this->getDataFolder());
+		@mkdir($this->cacheDir = $this->getDataFolder()."cache/");
+		$this->getServer()->getPluginManager()->registerInterface("pemapmodder\\pmmodpe\\JsPluginLoader");
+		$this->getServer()->getPluginManager()->loadPlugins($this->getServer()->getPluginPath(), ["pemapmodder\\pmmodpe\\JsPluginLoader"]);
+		$this->getLogger()->info("Finished loading plugins");
 	}
 }
